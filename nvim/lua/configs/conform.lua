80003704f3cf9a3ee682b538140c314bde11dc00
kushalchg <chapagainkushal74@gmail.com>
2024-10-05 23:49:30 +0545

    added some others also

---
+++ nvim/lua/configs/conform.lua
@@ -0,0 +1,20 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
+    go = { "goimports", "gofmt" },
+    typescript = { "prettier" },
+    javascript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascriptreact = { "prettier" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
