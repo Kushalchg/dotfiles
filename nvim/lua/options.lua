87a916059f0af5d06228660af35c79f8c3c11001
kushalchg <chapagainkushal74@gmail.com>
2024-10-06 08:56:50 +0545

    added spell checker

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,10 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "markdown", "latex" },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+  end,
+})
